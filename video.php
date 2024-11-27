c056962f05f6934bfbc0896ec89b5dd19b290a62
joshuasandovalb11 <erickjsandoval2000@gmail.com>
2024-11-26 16:03:17 -0800

    Agregacion del video

--- video.php
+++ video.php
@@ -9,39 +9,27 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=SUSE:wght@100..800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="index.css">
-    <title>Tienda Online</title>
+    <link rel="stylesheet" href="video.css">
+    <title>Video explicativo</title>
 </head>
+
 <body>
     <header class="header">
         <div class="logo">
             <span>A</span>
         </div>
-        <nav class="nav">
-            <a href="index.php">Inicio</a>
-            <a href="video.php">Creditos</a>
-        </nav>
+        <div class="actions">
+            <a class="btn btn-exit" href="index.php"> Regresar </a>
+        </div>
     </header>
 
-    <!-- Sección central -->
-    <main class="iconos-container container text-center my-5">
-        <div class="row justify-content-center">
-            <!-- Enlace a lista de productos -->
-            <div class="col-md-4">
-                <a href="productos.php" class="text-decoration-none text-dark">
-                    <i class="fas fa-list fa-5x icono"></i>
-                    <h3 class="mt-3">Lista de Productos</h3>
-                </a>
-            </div>
-            <!-- Enlace a agregar productos -->
-            <div class="col-md-4">
-                <a href="tienda.php" class="text-decoration-none text-dark">
-                    <i class="fas fa-plus-circle fa-5x icono"></i>
-                    <h3 class="mt-3">Agregar Productos</h3>
-                </a>
-            </div>
-        </div>
-    </main>
-    
+    <div class = "video-container">
+        <iframe width="600" height="400" 
+            src="https://www.youtube.com/embed/LXxqNZPohWo"
+            frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+    </div>
 </body>
 </html>
